ce4fe3fcd253cf1ba7da53756ac44ccc38e2290e
Diego Pinna <dp2907@nyu.edu>
2019-03-26 19:39:40 -0400

    managed to add constant line movement!

--- student_work/dpinna/personal_assignment02/moving_line.js
+++ student_work/dpinna/personal_assignment02/moving_line.js
@@ -22,26 +22,35 @@ function draw() {
   x1 = -width / 2 + 1
   x2 = width / 2
 
-  // stroke("red");
-  // if (a < lines_y.length) {
+  stroke("red");
+  if (frameCount > 50 && a <= lines_y.length) {
+    line(x1, lines_y[a], x2, lines_y[a])
+    a += 5
+  } else {
+    a = 0
+  }
+  if (frameCount > 100 && b <= lines_y.length) {
+    line(x1, lines_y[b], x2, lines_y[b])
+    b += 5
+  } else {
+    b = 0
+  }
+
+
+  // if (a <= lines_y.length) {
+  //   stroke("red");
   //   line(x1, lines_y[a], x2, lines_y[a])
-  //   a++
+  //   stroke("green");
+  //   line(x1, lines_y[a + 20], x2, lines_y[a + 20])
+  //   stroke("yellow");
+  //   line(x1, lines_y[a + 40], x2, lines_y[a + 40])
+  //   a += 5
   // } else {
+  //   console.log("else working");
+  //   x1 = -300;
+  //   x2 = -300;
   //   a = 0;
   // }
-
-
-  if (a < lines_y.length) {
-    stroke("red");
-    line(x1, lines_y[a], x2, lines_y[a])
-    stroke("green");
-    line(x1, lines_y[a + 20], x2, lines_y[a + 20])
-    stroke("yellow");
-    line(x1, lines_y[a + 40], x2, lines_y[a + 40])
-    a++
-  } else {
-    a = 0;
-  }
 
 
 
@@ -62,4 +71,18 @@ function draw() {
   fill("#1873C7")
   rect(-1500, -1000, 3000, 700)
 
+
+}
+
+if ((keyPressed == true) && (key == 'a')) {
+  frameRate(24);
+}else{
+  frameRate(30);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    frameRate(20);
+    console.log("happening");
+  }
 }
